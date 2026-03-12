e799d5ea8dfbd5f151dd86941b988dcbf57f588a
azizulhoq953 <azizulhoq2041@gmail.com>
2026-03-12 09:00:46 +0600

    assignments-issues-solved

--- src/constants/student/assignments.ts
+++ src/constants/student/assignments.ts
@@ -1,35 +1,3 @@
-// export type AssignmentStatus = 'PENDING' | 'IN_PROCESS' | 'COMPLETED';
-
-
-// export interface SubmittedAssignment {
-//     _id: string;
-//     fileAssignment: string;
-//     notes?: string;
-//     createdAt: string;
-// }
-
-// export interface Assignment {
-//     id: string;
-//     title: string;
-//     description: string;
-//     status: "PENDING" | "COMPLETED";
-//     dueDate: string;
-//     openDate?: string;
-//     totalPoint: number;
-//     attachment?: string;
-//     subject?: string;
-//     color?: string;
-//     submitAssignment?: SubmittedAssignment[];
-//     teacher?: {
-//         _id: string;
-//         firstName: string;
-//         lastName: string;
-//         profile: string;
-//     };
-//     userGroup?: { _id: string; name: string }[];
-//     userGroupTrack?: { _id: string; name: string };
-// }
-
 export type AssignmentStatus = 'PENDING' | 'IN_PROCESS' | 'COMPLETED';
 
 export interface SubmittedAssignment {
